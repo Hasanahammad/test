8c10bf9a395f13a47f999be64d44517fb66463e1
Hasan Ahammad <33357004+Hasanahammad@users.noreply.github.com>
2018-08-07 01:59:23 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Syntex</title>
+	<style>
+		.phpcoding{
+			width :900px;
+			margin :0 auto ;
+			background:#ddd;
+			padding: 20px;
+			min-height: 400px
+		}
+		.headeroption,
+		.footer{
+			background:#333;
+			color:#fff;
+			padding:  20px;
+			text-align: center;
+			text-transform:  uppercase;
+		}
+		.maincontent{
+			min-height: 200px;
+			text-align: center;
+			font-size:  30px;
+			text-transform:  uppercase;
+			margin-top:  140px;
+			}
+
+		.headeroption h2, .footer h2{margin: 0;
+
+		}
+	</style>
+
+
+
+</head>
+<body>
+	<div class="phpcoding">
+	<section class="headeroption">
+
+		<h2>php fundamental traning</h2>
+		
+	</section>
+
+	<section class="maincontent">
+	hello	</section>
+
+	<section class="footer">
+		<h2>www.trainging.com</h2>
+		
+	</section>
+</div>
+</body>
+</html>
